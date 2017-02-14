33aa58ed6dae707539fa8563d2660d307caf5367
Chauron5 <jloureirocal96@gmail.com>
2017-02-14 11:28:24 +0100

    Step1_FallingBlocksTest_3

--- src/main/java/tetris/Piece.java
+++ src/main/java/tetris/Piece.java
@@ -26,4 +26,22 @@ public class Piece {
     this.piece = new String(rotated);
     return this;
   }
+
+  public Piece rotateLeft() {
+    char[] previous = this.piece.toCharArray();
+    char[] rotated = this.piece.toCharArray();
+
+    rotated[8] = previous[0];
+    rotated[4] = previous[1];
+    rotated[0] = previous[2];
+    rotated[9] = previous[4];
+    rotated[5] = previous[5];
+    rotated[1] = previous[6];
+    rotated[10] = previous[8];
+    rotated[6] = previous[9];
+    rotated[2] = previous[10];
+
+    this.piece = new String(rotated);
+    return this;
+  }
 }
