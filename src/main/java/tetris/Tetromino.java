045fd912a9b89f4a44b4b541dacdfc8d493e6822
Chauron5 <jloureirocal96@gmail.com>
2017-02-20 17:19:40 +0100

    Step3-1-All_shape_instances

---
+++ src/main/java/tetris/Tetromino.java
@@ -0,0 +1,23 @@
+package tetris;
+
+public class Tetromino {
+  public static final Tetromino T_SHAPE = new Tetromino(new Piece(".T.\nTTT\n..."));
+
+  private Piece piece;
+
+    private Tetromino(Piece piece) {
+        this.piece = piece;
+    }
+
+    public String toString() {
+        return piece.toString();
+    }
+
+    public Tetromino rotateRight() {
+       return new Tetromino(piece.rotateRight());
+   }
+
+   public Tetromino rotateLeft() {
+       return new Tetromino(piece.rotateLeft());
+   }
+}
