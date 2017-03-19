410637a6aa2c6e76dff64ae3d038874c840f96f6
Chauron5 <jloureirocal96@gmail.com>
2017-03-19 19:40:53 +0100

    100% Coverage - Step7_ImprovingCoverageTest

---
+++ src/test/java/tetris/Step7_ImprovingCoverageTest.java
@@ -0,0 +1,49 @@
+package tetris;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Step7_ImprovingCoverageTest {
+
+  @Test
+  public void test_to_improve_coverage(){
+      Block b = new Block('X');
+      assertEquals(b.getValue(), 'X');
+
+      assertFalse(b.is_hollow_at(0,0));
+      assertFalse(b.is_hollow_at(1,0));
+      assertFalse(b.is_hollow_at(0,1));
+      assertTrue(b.is_hollow_at(1,1));
+
+      Piece p = new Piece("");
+      assertEquals(p.toString(), "");
+
+      Board board = new Board(6, 8);
+      Piece piece = new Piece("" +
+              ".X.\n" +
+              ".X.\n" +
+              "...\n");
+      board.tick();
+      assertEquals("" +
+              "........\n" +
+              "........\n" +
+              "........\n" +
+              "........\n" +
+              "........\n" +
+              "........\n", board.toString());
+      board.drop(piece);
+      board.tick();
+      board.tick();
+      board.tick();
+      board.tick();
+      board.drop(piece);
+      assertEquals("" +
+              "....X...\n" +
+              "....X...\n" +
+              "........\n" +
+              "........\n" +
+              "........\n" +
+              "........\n", board.toString());
+  }
+}
