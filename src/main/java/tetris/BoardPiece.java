71c3b3a426df3504ed55fcbf095bc2b73e69d78f
Chauron5 <jloureirocal96@gmail.com>
2017-03-28 19:41:01 +0200

    javadoc BoardPiece

--- src/main/java/tetris/BoardPiece.java
+++ src/main/java/tetris/BoardPiece.java
@@ -1,15 +1,35 @@
 package tetris;
 
+/**
+ * A class representing a piece of
+ * the board
+ */
 public interface BoardPiece {
-
+    /**
+     * Char thar represents the value of the piece
+     */
     final char EMPTY = '.';
 
+    /**
+    * @return String of the piece
+    */
     String toString();
 
+    /**
+    * @return with of the piece.
+    */
     int width();
 
+    /**
+    * @return height of the piece.
+    */
     int height();
 
+    /**
+    * @param i position(row) at the piece.
+    * @param j position(column) at the piece.
+    * @return true if the piece is not empty at  point (i,j).
+    */
     boolean is_hollow_at(int i, int j);
 
 }
